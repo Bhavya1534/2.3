f6d9c1f6d87112fe0a995b63cd05e11061d4184a
Bhavya1534 <139518528+Bhavya1534@users.noreply.github.com>
2023-10-03 23:54:49 +1100

    Update MyView.vue

--- src/MyView.vue
+++ src/MyView.vue
@@ -1,54 +1,103 @@
 <template>
-    <div>
-        <!-- 1. Template Syntax -->
-        <h1>{{ message }}</h1>
-        <div v-html="rawHTML"></div>
-        <div :id="elementId">{{ dynamicAttribute }}</div>
-    
-        <!-- 5. Class and Style Bindings -->
-        <div :class="{ 'active': isActive, 'error': hasError }" :style="{ color: textColor }">Class and Style Bindings</div>
+  <div>
+    <!-- 1. Template Syntax -->
+    <!-- a. Text Interpolation -->
+    <h1>{{ message }}</h1>
 
-        <!-- 6. List Rendering -->
-        <ul>
-          <li v-for="(item, key) in objectList" :key="key">{{ key }}: {{ item }}</li>
-          <li v-for="n in 5" :key="n">Item {{ n }}</li>
-          <template v-for="(item, index) in items" :key="index">
-            <li>{{ item }}</li>
-          </template>
-          <li v-for="(item, key) in filteredItems" :key="key">{{ item }}</li> <!-- d. v-for with v-if -->
-          <child-component v-for="(component, index) in components" :key="index" :name="component.name"></child-component>
-        </ul>
+    <!-- b. Raw HTML [v-html] -->
+    <div v-html="rawHTML"></div>
 
-        <!-- 7. Event Handling -->
-        <button @click="sayHello">Say Hello</button>
-        <button @click="inlineHandler">Inline Handler</button>
-        <button @click="methodHandler">Method Handler</button>
+    <!-- c. Attribute Bindings [v-bind:id] -->
+    <div :id="elementId">{{ dynamicAttribute }}</div>
 
-        <!-- 8. Form Input Bindings -->
-        <input type="text" v-model="textModel" />
-        <input type="checkbox" v-model="checkboxModel" />
-        <input type="radio" v-model="radioModel" :value="radioOption1" />
-        <select v-model="selectedOption">
-          <option value="option1">Option 1</option>
-          <option value="option2">Option 2</option>
-        </select>
-        <textarea v-model="textareaModel"></textarea>
+    <!-- d. JavaScript expressions inside syntax i.e.{{ }} -->
+    <p>{{ a + b }}</p>
 
-        <!-- 9. Watchers -->
-        <p>Reactive Value: {{ reactiveValue }}</p>
+    <!-- ... -->
 
-        <!-- 10. Components -->
-        <child-component :name="computedValue" @custom-event="handleCustomEvent"></child-component>
-        <!--11. Router-->
-         <!-- Navigation Links -->
-        <router-link to="/about">About</router-link>
+    <!-- 5. Class and Style Bindings -->
+    <!-- a. Binding HTML class [v-bind:class] -->
+    <div :class="{ 'active': isActive, 'error': hasError }">Class and Style Bindings</div>
 
-    </div>
+    <!-- b. Binding Inline Styles [v-bind:style] -->
+    <div :style="{ color: textColor }">Class and Style Bindings</div>
+
+    <!-- ... -->
+
+    <!-- 6. List Rendering -->
+    <!-- a. v-for with an Object -->
+    <ul>
+      <li v-for="(item, key) in objectList" :key="key">{{ key }}: {{ item }}</li>
+
+      <!-- b. v-for with a Range -->
+      <li v-for="n in 5" :key="n">Item {{ n }}</li>
+
+      <!-- c. v-for on <template> -->
+      <template v-for="(item, index) in items" :key="index">
+        <li>{{ item }}</li>
+      </template>
+
+      <!-- d. v-for with v-if -->
+      <li v-for="(item, key) in filteredItems" :key="key">{{ item }}</li>
+
+      <!-- e. v-for with a Component -->
+      <child-component v-for="(component, index) in components" :key="index" :name="component.name"></child-component>
+    </ul>
+
+    <!-- ... -->
+
+    <!-- 7. Event Handling -->
+    <!-- a. Inline Handlers -->
+    <button @click="sayHello">Say Hello</button>
+    <button @click="inlineHandler">Inline Handler</button>
+    <button @click="methodHandler">Method Handler</button>
+
+    <!-- ... -->
+
+    <!-- 8. Form Input Bindings -->
+    <!-- a. v-model with <input type="text">, <input type="checkbox">, <input type="radio">, <select> and <textarea> -->
+    <input type="text" v-model="textModel" />
+    <input type="checkbox" v-model="checkboxModel" />
+    <input type="radio" v-model="radioModel" :value="radioOption1" />
+    <select v-model="selectedOption">
+      <option value="option1">Option 1</option>
+      <option value="option2">Option 2</option>
+    </select>
+    <textarea v-model="textareaModel"></textarea>
+
+    <!-- b. v-model modifiers [.lazy , .number, .trim] -->
+    <input type="text" v-model.trim="textModel" />
+    <input type="checkbox" v-model.number="checkboxModel" />
+    <input type="radio" v-model.trim="radioModel" :value="radioOption1" />
+    <textarea v-model.lazy="textareaModel"></textarea>
+
+    <!-- ... -->
+
+    <!-- 9. Watchers -->
+    <p>Reactive Value: {{ reactiveValue }}</p>
+
+    <!-- 10. Components -->
+    <!-- a. Props -->
+    <child-component :name="computedValue" @custom-event="handleCustomEvent"></child-component>
+
+    <!-- b. Events [$emit] -->
+    <child-component @custom-event="handleCustomEvent"></child-component>
+
+    <!-- c. Slots -->
+    <child-component>
+      <template v-slot:default>Slot Content</template>
+    </child-component>
+
+    <!-- 11. Router -->
+    <!-- Navigation Links -->
+    <router-link to="/about">About</router-link>
+  </div>
 </template>
 
 <script setup>
 import { ref, computed, watch } from 'vue';
 import ChildComponent from './components/ChildComponent.vue';
+
 // 1. Template Syntax
 const message = ref('Hello, Vue!');
 const rawHTML = '<span style="color: red;">Red Text</span>';
@@ -97,8 +146,6 @@ const methodHandler = () => {
 };
 
 // 10. Components
-
-
 const handleCustomEvent = (data) => {
     console.log('Custom event received:', data);
 };
